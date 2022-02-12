994090e0e6813497be4d29ffa0cc2abb4141ea69
Noam Jegou Kasbawi <noamjk2010@gmail.com>
2022-02-12 11:28:11 +0100

    ajout des types canvas et ctx

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/** @type {HTMLCanvasElement} canvas  */
 let canvas=document.getElementById("canvas")
+/** @type {CanvasRenderingContext2D} ctx */
 let ctx = canvas.getContext("2d")
 ctx.fillRect(250 , 230 , 100 , 100)
